fdd7ca542fa51dd70a60b2b67fa5eb8b88640e63
kumar divyam <raikd1999@gmail.com>
2020-06-09 13:01:47 +0530

    npm scripts 1

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+   $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+            $("#dismis").click(function(){
+                $("#form").modal('hide');
+            });
+            $("#dismisss").click(function(){
+                $("#form").modal('hide');
+            });
+            $("#dsms").click(function(){
+                $("#loginModal").modal('hide');
+            });
+            $("#dsmss").click(function(){
+                $("#loginModal").modal('hide');
+            });
+            $("#login").click(function(){
+                $("#loginModal").modal('show');
+            });
+            $("#formshow").click(function(){
+                $("#form").modal('show');
+            });
